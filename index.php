f1d5dd50560af42e17ac3843d988cdf9a82d3dd8
DennisErazo1996 <dennis.erazo96@gmail.com>
2023-05-09 11:53:59 -0600

    Adición de sección del doctorado

--- index.php
+++ index.php
@@ -2,8 +2,23 @@
 <title>Maestrías | UNAG</title>
 <?php include_once('templates/header.php');?>
 <main>
-    <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" data-uk-slider="center: true; autoplay: true">
+    <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1"
+        data-uk-slider="center: true; autoplay: true">
         <ul class="uk-slider-items uk-grid-small uk-grid uk-grid-match" data-uk-height-viewport>
+            <li class="uk-width-1-1 uk-width-3-4@s">
+                <div class="uk-cover-container">
+                    <img src="images/slide-doctorado.jpeg" alt="" data-uk-cover>
+                    <div class="uk-position-bottom uk-panel uk-position-z-index">
+                        <div class="slideShow-item-text">
+                            <h4 class="uk-margin-remove-bottom">Doctorado en</h4>
+                            <h2 class="uk-margin-remove-top uk-margin-medium-bottom">Ciencias Agrarias</h2>
+                            <a href="doctorado-en-ciencias-agrarias" class="uk-button uk-button-primary">Más
+                                información</a>
+                        </div>
+                    </div>
+                    <div class="dark-glass"></div>
+                </div>
+            </li>
             <li class="uk-width-1-1 uk-width-3-4@s">
                 <div class="uk-cover-container">
                     <img src="images/mca-slide1.jpg" alt="" data-uk-cover>
@@ -11,7 +26,8 @@
                         <div class="slideShow-item-text">
                             <h4 class="uk-margin-remove-bottom">Maestría en</h4>
                             <h2 class="uk-margin-remove-top uk-margin-medium-bottom">Ciencias Agroalimentarias</h2>
-                            <a href="maestria-en-ciencias-agroalimentarias" class="uk-button uk-button-primary">Más información</a>
+                            <a href="maestria-en-ciencias-agroalimentarias" class="uk-button uk-button-primary">Más
+                                información</a>
                         </div>
                     </div>
                     <div class="dark-glass"></div>
@@ -23,8 +39,10 @@
                     <div class="uk-position-bottom uk-panel uk-position-z-index">
                         <div class="slideShow-item-text">
                             <h4 class="uk-margin-remove-bottom">Maestría en</h4>
-                            <h2 class="uk-margin-remove-top uk-margin-medium-bottom">Gestión de la Producción Animal Sostenible</h2>
-                            <a href="maestria-en-gestion-de-la-produccion-animal-sostenible" class="uk-button uk-button-primary">Más información</a>
+                            <h2 class="uk-margin-remove-top uk-margin-medium-bottom">Gestión de la Producción Animal
+                                Sostenible</h2>
+                            <a href="maestria-en-gestion-de-la-produccion-animal-sostenible"
+                                class="uk-button uk-button-primary">Más información</a>
                         </div>
                     </div>
                     <div class="dark-glass"></div>
@@ -36,8 +54,10 @@
                     <div class="uk-position-bottom uk-panel uk-position-z-index">
                         <div class="slideShow-item-text">
                             <h4 class="uk-margin-remove-bottom">Maestría en</h4>
-                            <h2 class="uk-margin-remove-top uk-margin-medium-bottom">Recursos Naturales y Producción Sostenible</h2>
-                            <a href="maestria-en-recursos-naturales-y-produccion-sostenible" class="uk-button uk-button-primary">Más información</a>
+                            <h2 class="uk-margin-remove-top uk-margin-medium-bottom">Recursos Naturales y Producción
+                                Sostenible</h2>
+                            <a href="maestria-en-recursos-naturales-y-produccion-sostenible"
+                                class="uk-button uk-button-primary">Más información</a>
                         </div>
                     </div>
                     <div class="dark-glass"></div>
@@ -50,20 +70,8 @@
                         <div class="slideShow-item-text">
                             <h4 class="uk-margin-remove-bottom">Máster Universitario en</h4>
                             <h2 class="uk-margin-remove-top uk-margin-medium-bottom">Biotecnología Agroalimentaria</h2>
-                            <a href="master-universitario-en-biotecnologia-agroalimentaria" class="uk-button uk-button-primary">Más información</a>
-                        </div>
-                    </div>
-                    <div class="dark-glass"></div>
-                </div>
-            </li>
-            <li class="uk-width-1-1 uk-width-3-4@s">
-                <div class="uk-cover-container">
-                    <img src="images/slide-doctorado.jpeg" alt="" data-uk-cover>
-                    <div class="uk-position-bottom uk-panel uk-position-z-index">
-                        <div class="slideShow-item-text">
-                            <h4 class="uk-margin-remove-bottom">Doctorado en</h4>
-                            <h2 class="uk-margin-remove-top uk-margin-medium-bottom">Ciencias Agrarias</h2>
-                            <a href="doctorado-en-ciencias-agrarias" class="uk-button uk-button-primary">Más información</a>
+                            <a href="master-universitario-en-biotecnologia-agroalimentaria"
+                                class="uk-button uk-button-primary">Más información</a>
                         </div>
                     </div>
                     <div class="dark-glass"></div>
@@ -71,8 +79,10 @@
             </li>
         </ul>
 
-        <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" data-uk-slidenav-previous data-uk-slider-item="previous"></a>
-        <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" data-uk-slidenav-next data-uk-slider-item="next"></a>
+        <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" data-uk-slidenav-previous
+            data-uk-slider-item="previous"></a>
+        <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" data-uk-slidenav-next
+            data-uk-slider-item="next"></a>
     </div>
 </main>
 <?php include_once('templates/footer.php');?>
